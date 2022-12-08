bdb686b0497dffc89cfa99c10984f203508e404f
Alonso <adroal08@gmail.com>
2022-12-08 10:06:52 -0600

    adding db

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 from views import views
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+
 
 
 #register of views
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/views")
-#app.register_blueprint(views, url_prefix='/proyecto')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/sitio.db'
 
-#conection to database
-app.config.from_object('config')
+#cursor to query to db
+db = SQLAlchemy(app)
+
+app.register_blueprint(views, url_prefix="/views")
+
+@app.route("/query")
+def query():
+    con=sqlite3.connect("database/sitio.db")
+    con.row_factory=sqlite3.Row
+    cur=con.cursor()
+    cur.execute("SELECT * FROM project")
+    data=cur.fetchall()
+    con.close()
+    return render_template('query.html', data=data)
 
 
 if __name__ == '__main__':
-    app.run(debug=False, port=8000)
+    app.run(debug=True, port=8000)
 
