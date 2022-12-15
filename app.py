c2e684f5e620273da71a2f8cc1077427bf5f36f2
Arlosaid <adroal08@gmail.com>
2022-12-15 10:28:51 -0600

    adding project and user route

--- app.py
+++ app.py
@@ -2,29 +2,37 @@ from flask import Flask, render_template
 from views import views
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
+import json
 
-
-
-#register of views
+# register of views
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/sitio.db'
 
-#cursor to query to db
+# cursor to query to db
 db = SQLAlchemy(app)
 
 app.register_blueprint(views, url_prefix="/views")
 
-@app.route("/query")
-def query():
-    con=sqlite3.connect("database/sitio.db")
-    con.row_factory=sqlite3.Row
-    cur=con.cursor()
-    cur.execute("SELECT * FROM project")
-    data=cur.fetchall()
+
+@app.route("/proyect_user")
+def proyect_user():
+    con = sqlite3.connect("database/sitio.db")
+    cur = con.cursor()
+    cur.execute(
+        "SELECT project_name FROM project WHERE project_name LIKE '%id%'")
+    data_projects = cur.fetchall()
+    proyectos = list(data_projects)
+    cur.execute("SELECT name,description,username,user_full_name FROM user JOIN user_role_association_table ON user.id=user_role_association_table.user_id JOIN role ON user_role_association_table.role_id=role.id")
+    data_users = cur.fetchall()
+    usuarios = list(data_users)
     con.close()
-    return render_template('query.html', data=data)
+    my_object = {
+        "projects": proyectos,
+        "users": usuarios
+    }
+    json_str = json.dumps(my_object)
+    return json_str
 
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
-
