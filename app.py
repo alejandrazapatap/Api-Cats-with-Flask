33a5a731b754249c535af779358e432a540c27ff
Alejandra Zapata <97615442+alejandrazapatap@users.noreply.github.com>
2022-12-16 14:24:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,4 @@ from flask import Flask
 from views import views
 
 
-#register of views
-app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/views")
-
-
-
-if __name__ == '__main__':
-    app.run(debug=False, port=8000)
 
